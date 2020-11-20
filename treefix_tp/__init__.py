559f1fd9c6f216fe8b2e759ac7f6d9f0769c0cec
Samuel Sledzieski <samsledje@gmail.com>
2020-11-20 12:47:55 -0500

    big refactoring, doesnt work right now

---
+++ treefix_tp/__init__.py
@@ -0,0 +1,22 @@
+#
+# Python module for TreeFix-TP
+#
+
+#=============================================================================
+# constants
+
+PROGRAM_NAME = "TreeFix-TP"
+PROGRAM_VERSION_MAJOR = 1
+PROGRAM_VERSION_MINOR = 1
+PROGRAM_VERSION_RELEASE = 0
+PROGRAM_VERSION = (PROGRAM_VERSION_MAJOR,
+                   PROGRAM_VERSION_MINOR,
+                   PROGRAM_VERSION_RELEASE)
+
+if PROGRAM_VERSION_RELEASE != 0:
+    PROGRAM_VERSION_TEXT = "%d.%d.%d" % (PROGRAM_VERSION_MAJOR,
+                                         PROGRAM_VERSION_MINOR,
+                                         PROGRAM_VERSION_RELEASE)
+else:
+    PROGRAM_VERSION_TEXT = "%d.%d" % (PROGRAM_VERSION_MAJOR,
+                                      PROGRAM_VERSION_MINOR)
